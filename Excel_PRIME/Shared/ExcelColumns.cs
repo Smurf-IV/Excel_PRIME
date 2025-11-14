296d51812484701761cafe63c69e87501bb96463
Simon Coghlan <smurf.iv@gmail.com>
2025-11-14 13:46:20 +0000

    - Small change

--- Excel_PRIME/Shared/ExcelColumns.cs
+++ Excel_PRIME/Shared/ExcelColumns.cs
@@ -36,32 +36,31 @@ internal static class ExcelColumns
     /// Convert ColumnNameRef - Character(s) into a Row - Column Excel Number eg A->1, B->2, AA -> 27
     /// </summary>
     public static (int rowExcel, int colExcel, char[] colName) GetRowColNumbers(this string columnRef) =>
-           columnRef.Length == 0 
+           columnRef.Length == 0
                ? (0, 0, [])
                : columnRef.AsSpan().GetRowColNumbers();
 
-       [MethodImpl(MethodImplOptions.AggressiveOptimization)]
-       public static (int rowExcel, int colExcel, char[] colName) GetRowColNumbers(this ReadOnlySpan<char> columnRefSpan)
-       {
-           int colExcel = -1;
-           int i = 0;
-           for (; i < columnRefSpan.Length; i++)
-           {
-               ref readonly char c = ref columnRefSpan[i];
-               int v = c - 'A';
-               if ((uint)v < 26u)
-               {
-                   colExcel = ((colExcel + 1) * 26) + v;
-               }
-               else
-               {
-                   break;
-               }
-           }
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+    public static (int rowExcel, int colExcel, char[] colName) GetRowColNumbers(this ReadOnlySpan<char> columnRefSpan)
+    {
+        int colExcel = -1;
+        int i = 0;
+        for (; i < columnRefSpan.Length; i++)
+        {
+            ref readonly char c = ref columnRefSpan[i];
+            if (c >= 'A')
+            {
+                colExcel = ((colExcel + 1) * 26) + (c - 'A');
+            }
+            else
+            {
+                break;
+            }
+        }
 
-           colExcel++; // Make it into the Excel 1 offset #
-           char[] colName = columnRefSpan.Slice(0, i).ToArray();
-           int rowExcel = columnRefSpan.Slice(i).IntParse();
-           return (rowExcel, colExcel, colName);
-       }
+        colExcel++; // Make it into the Excel 1 offset #
+        char[] colName = columnRefSpan.Slice(0, i).ToArray();
+        int rowExcel = columnRefSpan.Slice(i).IntParse();
+        return (rowExcel, colExcel, colName);
+    }
 }
